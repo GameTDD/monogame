d922433165138166340f4620e0ba1c54d0b8a97a
Julia Naomi <jnaomi@thoughtworks.com>
2019-02-03 02:40:37 -0200

    Interact with Region to change its state

---
+++ monogame.Testes/Objects/RegionTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using monogame.Objects;
+
+namespace monogame.Testes.Objects
+{
+    [TestFixture()]
+    public class RegionTest
+    {
+        Region region;
+        MouseState currentState;
+        MouseState previousState;
+
+        [TestFixtureSetUp()]
+        public void SetUpRegion()
+        {
+            region = new Region(10, 15, 20, 35);
+            currentState = new MouseState(20, 40, 0, ButtonState.Pressed, ButtonState.Released,
+                     ButtonState.Released, ButtonState.Released, ButtonState.Released);
+            previousState = new MouseState(25, 45, 0, ButtonState.Released, ButtonState.Released,
+                     ButtonState.Released, ButtonState.Released, ButtonState.Released);
+        }
+
+        [Test()]
+        public void InitialStateShouldBeZero()
+        {
+            Assert.That(region.State, Is.EqualTo(0));
+        }
+
+        [Test()]
+        public void InitialAreaShouldEqualRectangle()
+        {
+            Assert.That(region.Area, Is.EqualTo(new Rectangle(10, 15, 20, 35)));
+        }
+
+        [Test()]
+        public void StateDoesNotChangeAfterClick()
+        {
+            region.State = 0;
+            MouseState currentStateAux = new MouseState(1000, 400, 0, ButtonState.Pressed, ButtonState.Released,
+                     ButtonState.Released, ButtonState.Released, ButtonState.Released);
+            region.InteractWithRegionState(currentStateAux, previousState);
+            Assert.That(region.State, Is.EqualTo(0));
+        }
+
+        [Test()]
+        public void StateChangesTo1AfterClick()
+        {
+            region.InteractWithRegionState(currentState, previousState);
+            Assert.That(region.State, Is.EqualTo(1));
+        }
+    }
+}
---
+++ monogame/Objects/Region.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Handlers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace monogame.Objects
+{
+    public class Region
+    {
+        public int State { get; set; } 
+        public Rectangle Area { get; set; }
+
+        public Region(int x, int y, int width, int height)
+        {
+            State = 0;
+            Area = new Rectangle(x, y, width, height);
+        }
+
+        public void InteractWithRegionState(MouseState current, MouseState previous)
+        {
+            if (Regions.HasMouseClickedRegion(current, previous, Area))
+            {
+                State = 1;
+            }
+        }
+    }
+}
