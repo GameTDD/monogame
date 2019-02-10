6120e854feebdb6dac2b77e58d77bec9f245145f
Julia Naomi <jnaomi@thoughtworks.com>
2019-02-09 23:24:47 -0200

    Adds lines

---
+++ monogame.Testes/Objects/BoardConfig.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using monogame.Objects;
+
+namespace monogame.Testes.Objects
+{
+    [TestFixture()]
+    public class BoardConfig
+    {
+        Board gameBoard;
+
+        [TestFixtureSetUp()]
+        public void BoardTestSetUp()
+        {
+            gameBoard = new Board();
+        }
+
+        [Test()]
+        public void LinesAreReactangles()
+        {
+            Assert.That(gameBoard.lines[0], Is.InstanceOf<Rectangle>());
+        }
+
+        [Test()]
+        public void TestIfBoardHasOnly4Lines()
+        {
+            Assert.That(gameBoard.lines.Length, Is.EqualTo(4));
+        }
+
+        [Test()]
+        public void ThicknessAndLenghtAreCommonProps()
+        {
+            Assert.That(gameBoard.Thickness, Is.EqualTo(10));
+            Assert.That(gameBoard.Length, Is.EqualTo(300));
+        }
+    }
+}
---
+++ monogame/Objects/Board.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace monogame.Objects
+{
+    public class Board
+    {
+        public Rectangle[] lines { get; set; }
+        public int Thickness { get; set; }
+        public int Length { get; set; }
+
+
+        public Board()
+        {
+            lines = new Rectangle[4];
+            Thickness = 10;
+            Length = 300;
+        }
+    }
+}
