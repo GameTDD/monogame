bf71d9ab001d79770f185659aba2cc91494b1399
naomijub <jnboeira@outlook.com>
2019-05-12 00:42:35 -0300

    Win Manager

---
+++ monogame.Testes/Handlers/WinStateManagerTest.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using monogame.Objects;
+
+namespace monogame.Testes.Handlers
+{
+    [TestFixture()]
+    public class WinStateManagerTest
+    {
+        Region[] regions;
+
+        [TestFixtureSetUp()]
+        public void TestSetup()
+        { 
+            regions = new Region[9] {
+                new Region(100, 100, 94, 94, null),
+                new Region(206, 100, 88, 94, null),
+                new Region(306, 100, 94, 94, null),
+                new Region(100, 206, 94, 88, null),
+                new Region(206, 206, 88, 88, null),
+                new Region(306, 206, 94, 88, null),
+                new Region(100, 306, 94, 94, null),
+                new Region(206, 306, 88, 94, null),
+                new Region(306, 306, 94, 94, null)
+            };
+        }
+
+        [Test()]
+        public void AllRegionsWith0NoVictoryPlayer()
+        {
+            Assert.That(WinStateManager.WhichPlayerWon(regions), Is.EqualTo(0));
+        }
+
+        [Test()]
+        public void Row1HasWonP1()
+        {
+            regions[0].State = 1;
+            regions[1].State = 1;
+            regions[2].State = 1;
+            Assert.That(WinStateManager.WhichPlayerWon(regions), Is.EqualTo(1));
+        }
+
+        [Test()]
+        public void Row2HasWonP2()
+        {
+            TestSetup();
+            regions[3].State = -1;
+            regions[4].State = -1;
+            regions[5].State = -1;
+            Assert.That(WinStateManager.WhichPlayerWon(regions), Is.EqualTo(-1));
+        }
+
+        [Test()]
+        public void Col1HasWonP1()
+        {
+            TestSetup();
+            regions[0].State = 1;
+            regions[3].State = 1;
+            regions[6].State = 1;
+            Assert.That(WinStateManager.WhichPlayerWon(regions), Is.EqualTo(1));
+        }
+
+        [Test()]
+        public void Col2HasWonP2()
+        {
+            TestSetup();
+            regions[1].State = -1;
+            regions[4].State = -1;
+            regions[7].State = -1;
+            Assert.That(WinStateManager.WhichPlayerWon(regions), Is.EqualTo(-1));
+        }
+    }
+}
---
+++ monogame/Objects/WinStateManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace monogame.Objects
+{
+    public class WinStateManager
+    {
+        public static int WhichPlayerWon(Region[] regions)
+        {
+            int row1 = HasWon( new Region[] { regions[0], regions[1], regions[2] });
+            int row2 = HasWon( new Region[] { regions[3], regions[4], regions[5] });
+            int row3 = HasWon( new Region[] { regions[6], regions[7], regions[8] });
+            int col1 = HasWon(new Region[] { regions[0], regions[3], regions[6] });
+            return DetermineWinner(row1, row2, row3, col1);
+        }
+
+        public static int HasWon(Region[] regions) {
+            return ((regions[0].State == 1 && regions[1].State == 1 && regions[2].State == 1)
+                || (regions[0].State == -1 && regions[1].State == -1 && regions[2].State == -1))
+                ? regions[0].State
+                : 0;
+        }
+
+        public static int DetermineWinner(int row1, int row2, int row3, int col1)
+        {
+            if (row1 != 0)
+            {
+                return row1;
+            }
+            else if (row2 != 0)
+            {
+                return row2;
+            }
+            else if (row3 != 0)
+            {
+                return row3;
+            }
+            return col1;
+        }
+    }
+}
